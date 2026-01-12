403d3dd115bb902a080a8dbed825e251e4591278
LoisClinton <141070136+LoisClinton@users.noreply.github.com>
2026-01-12 10:14:52 +0000

    feat: add login implementation with firebase authentication and redirect

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,6 +1,22 @@
+import { auth } from "@/src/firebase/firebaseConfig";
+import { router } from "expo-router";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
 import { Button, Text, TextInput, View } from "react-native";
 
 export default function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email.trim(), password);
+      router.replace("/");
+    } catch (e: any) {
+      console.log("Login failed", e.message);
+    }
+  };
+
   return (
     <View style={{ padding: 16, gap: 12 }}>
       <Text style={{ fontSize: 24, fontWeight: "600" }}>Login Screen</Text>
@@ -8,16 +24,20 @@ export default function LoginScreen() {
       <TextInput
         placeholder="Email"
         autoCapitalize="none"
+        value={email}
+        onChangeText={setEmail}
         style={{ borderWidth: 1, padding: 12, borderRadius: 8 }}
       />
 
       <TextInput
         placeholder="Password"
         secureTextEntry
+        value={password}
+        onChangeText={setPassword}
         style={{ borderWidth: 1, padding: 12, borderRadius: 8 }}
       />
 
-      <Button title="Log in" />
+      <Button title="Log in" onPress={onLogin} />
     </View>
   );
 }
