eee68c1a8afe41b630845c1f7d900990162d04c1
LoisClinton <141070136+LoisClinton@users.noreply.github.com>
2026-01-12 09:43:18 +0000

    chore: add firebase config

---
+++ src/firebase/firebaseConfig.ts
@@ -0,0 +1,17 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
